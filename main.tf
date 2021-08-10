490f3211c5568fb160a55f76a2c69d629748035c
TirthankarSingh <53862702+TirthankarSingh@users.noreply.github.com>
2021-08-10 08:22:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,9 @@
 terraform {
     required_version = "~> 1.0.3" // added latest version
-    required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-     }
-    }
+#     required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 3.0"
+#      }
+#     }
    }
-
-provider "aws" {
-       region = "us-east-1" 
-        profile = "default"
-	}
-
-resource "aws_instance" "Tirth" {
-	ami = "ami-0c2b8ca1dad447f8a" 
-	instance_type = "t2.micro"
-}
